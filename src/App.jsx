fe45f8b58f9d4b02368bfaaccc6341aae568be40
iwankrisnawan <iwankrisnawan03@gmail.com>
2023-01-22 12:02:26 +0800

    add splash screen page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import Splash_screen from "./pages/Splash_screen";
 
 export default function App() {
   return (
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/splash-screen" element ={<Splash_screen/>}/>
       </Routes>
   );
 }
