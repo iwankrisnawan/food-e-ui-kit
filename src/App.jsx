c4d03c3551d0e4355a822f48ddfab2228fa6a397
iwankrisnawan <iwankrisnawan03@gmail.com>
2023-01-22 19:46:07 +0800

    add page onboarding1

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // pages
 import Home from "./pages/Home";
 import Splash_screen from "./pages/Splash_screen";
+import Onboarding1 from "./pages/Onboarding_1";
 
 export default function App() {
   return (
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/splash-screen" element ={<Splash_screen/>}/>
+        <Route path="/onboarding1" element ={<Onboarding1/>}/>
       </Routes>
   );
 }
