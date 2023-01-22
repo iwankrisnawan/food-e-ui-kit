0d06ab9ef2433695c227f4949e30b777782d7dd9
iwankrisnawan <iwankrisnawan03@gmail.com>
2023-01-22 12:27:18 +0800

    update color tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      primaryLight: "#67E5CE",
+      primaryDefault: "#2FDBBC",
+      primaryDark: "#1EAE94",
+      secondaryLight: "#FAAC52",
+      secondaryDefault: "#F99928",
+      secondaryDark: "#DF7B07",
+      alertSuccess: "#4FE178",
+      alertWarning: "#F1C75D",
+      alertError: "#F77272",
+    },
   },
   plugins: [],
 }
