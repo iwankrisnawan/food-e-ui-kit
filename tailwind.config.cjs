069a2cb6865e8e906dc4ccfd243df33d978a01db
iwankrisnawan <iwankrisnawan03@gmail.com>
2023-01-28 23:56:21 +0800

    update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      primaryLight: "#67E5CE",
-      primaryDefault: "#2FDBBC",
-      primaryDark: "#1EAE94",
-      secondaryLight: "#FAAC52",
-      secondaryDefault: "#F99928",
-      secondaryDark: "#DF7B07",
-      alertSuccess: "#4FE178",
-      alertWarning: "#F1C75D",
-      alertError: "#F77272",
+    extend: {
+      colors: {
+        customLight: "#F4F4F4",
+        customGray: "#A3A3A3",
+        customDark: "#2D2D2D",
+        primaryLight: "#67E5CE",
+        primaryDefault: "#2FDBBC",
+        primaryDark: "#1EAE94",
+        secondaryLight: "#FAAC52",
+        secondaryDefault: "#F99928",
+        secondaryDark: "#DF7B07",
+        alertSuccess: "#4FE178",
+        alertWarning: "#F1C75D",
+        alertError: "#F77272",
+      },
     },
   },
   plugins: [],
-}
+};
